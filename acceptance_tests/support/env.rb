4ea3ec8bc290f3e22207207ce959e2ef98e859c5
Jason Carter <jccarte@gmail.com>
2015-08-03 08:59:30 -0400

    Added parallel_test gem and associated changes

--- acceptance_tests/support/env.rb
+++ acceptance_tests/support/env.rb
@@ -3,7 +3,8 @@ require 'capybara/cucumber'
 require 'rspec/expectations'
 
 Capybara.default_driver = :selenium
+Capybara.server_port = 10000 + ENV['TEST_ENV_NUMBER'].to_i
 Capybara.register_driver :selenium do |app|
-Capybara::Selenium::Driver.new(app, :browser => :chrome,)
+  Capybara::Selenium::Driver.new(app, :browser => :chrome,)
 end
 #Capybara.ignore_hidden_elements = false
