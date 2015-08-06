a756188910fd6931384828450cb8526d6e87c167
Kris <kris.clemente@lootcrate.com>
2015-08-05 18:52:32 -0700

    Fixing some typos in the file names.

--- acceptance_tests/step_definitions/level_up.rb
+++
@@ -1,9 +0,0 @@
-When /the user selects a level up (.*) month subscription for the (.*) crate/ do |months, product|
-  $test.current_page.select_level_up(product,months)
-end
-
-Then /the new level up subscription should be added to the user account/ do
-  step "the user visits the my account page"
-  $test.current_page.verify_levelup_subscription_added
-end
-
