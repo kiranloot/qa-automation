3b6d4b76056d3ddc082d831de82b803262179419
Kris <kris.clemente@lootcrate.com>
2015-09-10 12:12:58 -0700

    Using an rspec match instead of eq to check affiliate redirect. Needed to add an escaped version of the url to deal with the "?".

--- acceptance_tests/support/acceptance_objects/affiliate_object.rb
+++ acceptance_tests/support/acceptance_objects/affiliate_object.rb
@@ -1,7 +1,8 @@
 class Affiliate
-  attr_accessor :name, :redirect_url
+  attr_accessor :name, :redirect_url, :redirect_url_escaped
   def initialize
     @name = 'affiliate_test'
     @redirect_url = 'google.com'
+    @redirect_url_escaped = 'google.com'
   end
 end
