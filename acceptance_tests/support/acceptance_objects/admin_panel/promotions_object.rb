e98645bb5e2380bafda428c6e539af54ec208030
Kris <kris.clemente@lootcrate.com>
2015-09-16 11:46:02 -0700

    Refactoring the promotions object.

--- acceptance_tests/support/acceptance_objects/admin_panel/promotions_object.rb
+++ acceptance_tests/support/acceptance_objects/admin_panel/promotions_object.rb
@@ -9,33 +9,79 @@ class AdminPromotionsPage < AdminPage
   def create_promotion(type)
     find_link("New Promotion").click
     rand_code = (0...8).map { ('a'..'z').to_a[rand(26)] }.join
-    fill_in("promotion_description", :with => "Promotion Created by Automation")
-    find(:id, "promotion_starts_at").click
-    find(:css, "div.ui-datepicker-group-first").find_link("1").click
-    find(:id,"s2id_promotion_trigger_event").click
-    fill_in("s2id_autogen3", :with => "SIGNUP")
-    find(:css, "div.select2-result-label").click
-    find(:id, "select_all").click
-    fill_in("promotion_adjustment_amount", :with => "10")
-    fill_in("promotion_coupon_prefix", :with => rand_code)
+    fill_in_promotion_description("Promotion Created by Automation")
+    select_start_date("1")
+    select_trigger_event("SIGNUP")
+    select_all_plans
+    fill_in_promotion_adjustment_amount(10)
+    fill_in_promotion_coupon_prefix(rand_code)
     case type
     when 'multi use'
-      fill_in("promotion_name", :with => "Multi Use Automation Promo #{rand_code}")
-      fill_in("promotion_conversion_limit", :with => "10")
-      find(:id, "btn-submit").click
-      wait_for_ajax
+      fill_in_promotion_name("Multi Use Automation Promo #{rand_code}")
+      fill_in_promotion_conversion_limit("10")
+      click_create_promotion
     when 'one time use'
-      fill_in("promotion_name", :with => "One Time Automation Promo #{rand_code}")
-      find(:id, "promotion_one_time_use").click
-      fill_in("char_length", :with => "15")
-      fill_in("quantity", :with => "1")
-      find(:id, "btn-submit").click
-      wait_for_ajax
-      click_promotions
-      display_coupons_for_promo_by_prefix(rand_code) 
-      rand_code = get_first_coupon_code
+      fill_in_promotion_name("One Time Automation Promo #{rand_code}")
+      click_one_time_use_checkbox
+      fill_in_character_length("15")
+      fill_in_quantity("1")
+      click_create_promotion
+      rand_code = get_one_time_promo_code(rand_code)
     end
     return rand_code
+  end
+
+  def fill_in_promotion_name(name)
+    fill_in("promotion_name", :with => name)
+  end
+
+  def click_one_time_use_checkbox
+    find(:id, "promotion_one_time_use").click
+  end
+
+  def fill_in_character_length(length)
+    fill_in("char_length", :with => length)
+  end
+
+  def fill_in_quantity(quantity)
+    fill_in("quantity", :with => quantity)
+  end
+
+  def fill_in_promotion_conversion_limit(limit)
+    fill_in("promotion_conversion_limit", :with => limit)
+  end
+
+  def fill_in_promotion_description(description)
+    fill_in("promotion_description", :with => description)
+  end
+
+  def fill_in_promotion_coupon_prefix(prefix)
+    fill_in("promotion_coupon_prefix", :with => prefix)
+  end
+
+  def fill_in_promotion_adjustment_amount(amount)
+    fill_in("promotion_adjustment_amount", :with => amount) 
+  end
+
+  def select_start_date(date)
+    find(:id, "promotion_starts_at").click
+    find(:css, "div.ui-datepicker-group-first").find_link(date).click
+  end
+
+  def select_trigger_event(event)
+    find(:id,"s2id_promotion_trigger_event").click
+    fill_in("s2id_autogen3", :with => event)
+    find(:css, "div.select2-result-label").click
+  end
+
+  def select_all_plans
+    find(:id, "select_all").click
+  end
+
+  def click_create_promotion
+    find(:id, "btn-submit").click
+    wait_for_ajax
+    assert_text("Promotion Details")
   end
 
   def filter_promo_by_coupon_code(code)
@@ -58,5 +104,11 @@ class AdminPromotionsPage < AdminPage
         page.driver.browser.navigate.refresh
       end
     end
+  end
+
+  def get_one_time_promo_code(rand_code)
+      click_promotions
+      display_coupons_for_promo_by_prefix(rand_code) 
+      get_first_coupon_code
   end
 end
