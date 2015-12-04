0331c5cf68fed10482dea63212581483776f003f
JeffreyWu <jeffreywu@lc-jeffreywu.local>
2015-12-03 17:52:10 -0800

    join modal

--- acceptance_tests/support/acceptance_objects/page_object.rb
+++ acceptance_tests/support/acceptance_objects/page_object.rb
@@ -59,5 +59,20 @@ class Page
     find_button("RESET PASSWORD").click
     $test.user.toggle_password
   end
+
+  def modal_signup(email, password, test_data)
+    wait_for_ajax
+    page.find_link("Log In").click
+    page.find_link("Forgot Password?")
+    page.find_link("Join")
+    page.has_content?("Welcome Back!")
+    page.find(:css, test_data["locators"]["modal_join"]).click
+    page.has_content?("Join the Looter community!")
+    fill_in("new_user_email_modal", :with => email)
+    page.find_field("new_user_email_modal").value
+    page.find_link("finish_step_one").click
+    fill_in("new_user_password_modal",:with => password)
+    page.find_button("create_account_modal").click
+  end
   
 end
