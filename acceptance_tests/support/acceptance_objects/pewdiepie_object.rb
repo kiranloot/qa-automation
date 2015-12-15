ea9b611315305214e9e420496153e89cd26b8249
JeffreyWu <jeffreywu@lc-jeffreywu.local>
2015-12-14 16:04:44 -0800

    referer pages

---
+++ acceptance_tests/support/acceptance_objects/pewdiepie_object.rb
@@ -0,0 +1,15 @@
+require_relative "page_object"
+require_relative "wait_module"
+require "capybara/cucumber"
+require "pry"
+class PewdiepiePage < Page
+  include Capybara::DSL
+  include WaitForAjax
+  def initialize
+    super
+    @page_type = "pewdiepie"
+    setup
+  end
+
+  
+end
