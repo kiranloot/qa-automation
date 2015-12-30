47659b65cd34d07bfc540053ec430245e9cb1ea9
Chris Rothman <chrisrothman@lc-chrisrothman.local>
2015-12-29 17:44:51 -0800

    Add page load validation for TradeChat referrer.

--- acceptance_tests/step_definitions/referrer_steps.rb
+++ acceptance_tests/step_definitions/referrer_steps.rb
@@ -5,3 +5,7 @@ end
 Then(/^user should see boogie2988 stuff$/) do
   expect(page).to have_content('MY TOP 3 STUPIDLY AWESOME CRATES')
 end
+
+Then(/^user should see tradechat stuff$/) do
+  expect(page).to have_content("I've gotten some amazing stuff from Loot Crate! Here are my favorites!")
+end
