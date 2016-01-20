6a2da473b3160dcb0e024a6b4ed6a17e4ffbac8c
Thomas Lawler <thomas.lawler@lootcrate.com>
2016-01-20 11:35:58 -0800

    Added qa environment validation for database url

--- acceptance_tests/support/acceptance_objects/qa_env_validator.rb
+++ acceptance_tests/support/acceptance_objects/qa_env_validator.rb
@@ -13,5 +13,10 @@ module QAEnvironmentValidator
       raise "===\nRecurly Subdomain is configured to prod environment\n==="
     end
     puts "Recurly verified"
+
+    if (config['DATABASE_URL'] == 'postgres://uepc1csoapak51:p2sigo8p4eeeqm7lgbb2mgkm8bq@ec2-174-129-245-179.compute-1.amazonaws.com:5492/daj5d11plkpskh')
+      raise "===\nDatabase URL is configured to prod's\n==="
+    end
+    puts "Database verified"
   end
 end
