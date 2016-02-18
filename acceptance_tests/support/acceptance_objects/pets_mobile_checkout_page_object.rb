0df5c85c8d9b942f274ccd51c52f67c2d1f6ab64
Kris <kris.clemente@lootcrate.com>
2016-02-18 14:53:22 -0800

    Fixing pets mobile checkouts

---
+++ acceptance_tests/support/acceptance_objects/pets_mobile_checkout_page_object.rb
@@ -0,0 +1,21 @@
+require_relative "mobile_checkout_page_object"
+
+class PetsMobileCheckoutPage < MobileCheckoutPage
+  def initialze
+    super
+    @page_type = "pets_checkout"
+    setup
+  end
+
+  def select_shirt_size(size)
+    #stub
+  end
+
+  def select_pet_shirt_size(size)
+    select "#{size}", :from => "option_type_shirt"
+  end
+
+  def select_pet_collar_size(size)
+    select "#{size}", :from => "option_type_collar"
+  end
+end
