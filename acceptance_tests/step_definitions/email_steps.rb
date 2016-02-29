06e4d1cb242997f48ef67a171dca6a3a3ad73199
Thomas Lawler <thomas.lawler@lootcrate.com>
2016-02-29 15:47:37 -0800

    Updated password reset test to improve stability

--- acceptance_tests/step_definitions/email_steps.rb
+++ acceptance_tests/step_definitions/email_steps.rb
@@ -3,7 +3,7 @@
 #WHENS
 When /^The user clicks on the reset link in their email$/ do
   email = $test.mailinator.download_first_email($test.user.email)
-  reset_link = /(http:\/\/mandrillapp.com\/track\/click.*)>/.match(email.body)
+  reset_link = /(http:\/\/mandrillapp.com\/track\/click\/\d*\/#{ENV['SITE']}.*)>/.match(email.body)
   visit(reset_link)
   $test.current_page = PasswordResetPage.new
 end
@@ -12,4 +12,3 @@ end
 Then /the user should receive an? (.*?) email/ do |type|
   $test.verify_email(type)
 end
-
