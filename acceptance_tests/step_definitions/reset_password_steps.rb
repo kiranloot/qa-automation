252275997b3e64adc200944047572b94c231ed93
Kris <kris.clemente@lootcrate.com>
2016-03-01 18:29:46 -0800

    Commiting a missing step in the step definition for reseting password.

--- acceptance_tests/step_definitions/reset_password_steps.rb
+++ acceptance_tests/step_definitions/reset_password_steps.rb
@@ -6,6 +6,7 @@ end
 #THENS
 Then /^the user should be able to login with their new password$/ do
   $test.current_page.log_out
+  step "the user visits the signup page"
   $test.current_page.log_in_or_register
   step "the user should be logged in"
 end
