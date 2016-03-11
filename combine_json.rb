44ec5cc87b6077fedcc3564dea8492abe5e5324f
Kris <kris.clemente@lootcrate.com>
2016-03-11 14:44:04 -0800

    commiting a file close as well as a new output.json for testing.

--- combine_json.rb
+++ combine_json.rb
@@ -3,4 +3,5 @@ require 'json'
 json = Dir['./reports/report*.json'].map { |f| File.read(f).length >= 2 ? JSON.parse(File.read(f)) : nil }.flatten
 File.open("output.json","w") do |f|
   f.write(json.to_json)
+  f.close
 end
