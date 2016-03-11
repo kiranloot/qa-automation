902c9e8e581682b0982714838bf9e0af2611aff9
Kris <kris.clemente@lootcrate.com>
2016-03-11 15:03:27 -0800

    Attempting to force UTF-8 encoding.

--- combine_json.rb
+++ combine_json.rb
@@ -1,6 +1,6 @@
 require 'json'
 
-json = Dir['./reports/report*.json'].map { |f| File.read(f).length >= 2 ? JSON.parse(File.read(f)) : nil }.flatten
+json = Dir['./reports/report*.json'].map { |f| File.read(f).length >= 2 ? JSON.parse(File.read(f, :encoding => 'UTF-8')) : nil }.flatten
 File.open("output.json","w") do |f|
   f.write(json.to_json)
   f.close
