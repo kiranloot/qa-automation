aefeb5f8f3029ab16ccb899319c71721accde107
Thomas Lawler <thomas.lawler@lootcrate.com>
2016-05-18 09:42:55 -0700

    Adds conditions to ensure the for her + tee bundle is always set to womens waist.

--- acceptance_tests/support/acceptance_objects/level_up_subscribe_page_object.rb
+++ acceptance_tests/support/acceptance_objects/level_up_subscribe_page_object.rb
@@ -94,7 +94,10 @@ class LevelUpSubscribePage < SubscribePage
       size_section = find("section#section-lu-sizes")
       choose_gender(subscription.gender) if size_section.text.include?('MENS WOMENS')
       find("#sizes-btn-shirt-#{subscription.gender}-#{subscription.sizes[:shirt]}").click if size_section.text.include?('SHIRT SIZE')
-      find("#sizes-btn-waist-#{subscription.gender}-#{subscription.sizes[:waist]}").click if size_section.text.include?('WAIST SIZE')
+      if size_section.text.include?('WAIST SIZE')
+        subscription.gender = 'womens' if subscription.product == 'for her + tee'
+        find("#sizes-btn-waist-#{subscription.gender}-#{subscription.sizes[:waist]}").click
+      end
       wait_for_ajax
     end
   end
